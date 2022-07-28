6be1821a249e38d2a1c91b7aa8240903fbb4a4b3
hyeji07 <zindex_ann@naver.com>
2022-07-28 19:38:53 +0900

    소플 예제

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <NotificationList />
+    </React.StrictMode>
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
